46e9efa3f5fdf29ad64378feb6eaa41c020e3828
The EasyLearn Academy <theeasylearn@users.noreply.github.com>
2022-09-07 18:51:46 +0530

    lesson1

---
+++ variables.py
@@ -0,0 +1,17 @@
+print("working with variables in python")
+#create variable in python 
+# syntax 
+# variable-name = value 
+name = "the easylearn academy" #create variable name and it has the easylearn academy
+age = 38
+weight = 74.99
+gender = True 
+print(name)
+print("weight ",weight,"age ",age)
+print(gender)
+age = 'thirty eight'
+print(age)
+gender = "male"
+print(gender)
+age = 38
+print(age)
