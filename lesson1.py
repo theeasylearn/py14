50032e8652a14eba077f5b6196f17402b52299dd
The EasyLearn Academy <theeasylearn@users.noreply.github.com>
2022-09-14 18:46:04 +0530

    print with sid

--- lesson1.py
+++ lesson1.py
@@ -1,1 +1,13 @@
-print("Hello world.....")
+#first program in python 
+'''
+Author : Ankit Patel
+date : 14-09-2022
+simple python program
+'''
+print('Learning python')
+print("Python is very useful and multipurpose languange.....")
+print(""" we are learning python at
+      the easylearn academy
+      105, eva surbhi, opp akshwarwadi temple,
+      waghawadi road, bhavnagar - 364001
+""")
