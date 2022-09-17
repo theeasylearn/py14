085b0f2dbcfe4bafc0e8d86661f7e8442b9d7b42
The EasyLearn Academy <theeasylearn@users.noreply.github.com>
2022-09-17 07:02:24 +0530

    list and tuple

---
+++ list.py
@@ -0,0 +1,8 @@
+fruits=['mango','apple','oragne','grape']
+print(fruits)
+print(fruits[2])
+temp=fruits[3]
+print(temp)
+print(fruits[0:3])
+print(fruits+fruits)
+print(fruits*5)
---
+++ string2.py
@@ -0,0 +1,10 @@
+# example of string
+name="param patel "
+company='the easylearn '
+
+print(name)
+print(company)
+print(name[0:5])
+print(name[6:])
+print(name+company+name)
+print(name*10)
