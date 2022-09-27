39fd7fe76dbc75f3f2d41146fb9868f449e53ae4
The EasyLearn Academy <theeasylearn@users.noreply.github.com>
2022-09-27 19:07:26 +0530

    loop

---
+++ loop1.py
@@ -0,0 +1,8 @@
+#write a program to print numbers between 1 to 100
+# 1 2 3 4 5 6 ......... 100
+number = 1
+while number<=100:
+    print(number,end=' ')
+    number = number + 1 #3
+print("loop finished.....")   
+print("Good bye...")
---
+++ loop2.py
@@ -0,0 +1,11 @@
+#write a program to print following pattern 
+'''
+2   4   6   8   ....... 200
+'''
+count = 2
+while count<=200:
+    print(count,end=' ') # 2
+    count=count + 2 #4
+    
+print("loop finished.....")
+print('good bye.....')
