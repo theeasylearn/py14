5c13e9297ddbd49c0eb01fb15e9c6691388089c0
The EasyLearn Academy <theeasylearn@users.noreply.github.com>
2022-10-14 19:15:01 +0530

    functions in python

---
+++ function1.py
@@ -0,0 +1,15 @@
+# first example of user defined function
+# def function-name():
+#   statements 
+#without return without argument function
+def PrintLine():
+    print("#"*100)
+    
+#function-name()
+PrintLine()
+print("the easylearn academy")
+PrintLine()
+print("Bhavnagar - 364001")
+PrintLine()
+
+    
---
+++ function2.py
@@ -0,0 +1,15 @@
+# second example of user defined function
+# def function-name():
+#   statements 
+#without return value with argument function
+def PrintLetter(AnyLetter,count):
+    print(AnyLetter*count)
+    
+#function-name()
+PrintLetter('$',100)
+print("the easylearn academy")
+PrintLetter('^',150)
+print("Bhavnagar - 364001")
+PrintLetter('_',180)
+
+    
