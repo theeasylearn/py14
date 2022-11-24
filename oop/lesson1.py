2dce3100752fa751cd6091a84b0c0a19d8d49698
The EasyLearn Academy <theeasylearn@users.noreply.github.com>
2022-11-24 17:36:27 +0530

    OOP

---
+++ oop/lesson1.py
@@ -0,0 +1,22 @@
+#creating class (define class)
+class Person:
+    def eat(self):
+        print('I can eat')
+    def sleep(self):
+        print('I can sleep')
+    def walk(self):
+        print('I can walk')
+        
+
+#create object 
+#object-name = class()
+ram = Person()
+ram.walk()
+ram.sleep()
+ram.eat()
+
+krishna = Person()
+krishna.walk()
+krishna.sleep()
+krishna.eat()
+    
